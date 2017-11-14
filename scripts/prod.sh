dae01d810d09a91c3eb88b3d776a290dd2ffe49c
Ryan Barth <ryan@barth.tech>
2017-11-14 22:18:16 +0000

    do not run prod in foreground

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,15 +1,15 @@
 #1amadmin!/bin/bash
 NAME=ellie_2
 
-function finish {
-  echo "Stopping jobs..."
-  jobs -p | xargs kill
-  echo "Stoping containsers..."
-  docker rm -f ${NAME}_web ${NAME}_worker
-  echo "Goodbye!"
-}
+#function finish {
+  #echo "Stopping jobs..."
+  #jobs -p | xargs kill
+  #echo "Stoping containsers..."
+  #docker rm -f ${NAME}_web ${NAME}_worker
+  #echo "Goodbye!"
+#}
 
-trap finish EXIT
+#trap finish EXIT
 
 docker build -t ${NAME}_web -f Dockerfile.web . &&
 docker build -t ${NAME}_worker -f Dockerfile.worker . &&
@@ -17,7 +17,7 @@ docker run -d \
   --name ${NAME}_web \
   --env-file .env.docker \
   --net host \
-  ${NAME}_web -p 9293
+  ${NAME}_web
 docker run -d \
   --name ${NAME}_worker \
   --env-file .env.docker \
@@ -25,7 +25,7 @@ docker run -d \
   -e QUEUE=default \
   ${NAME}_worker
 
-docker logs -f ${NAME}_web &
-docker logs -f ${NAME}_worker &
+#docker logs -f ${NAME}_web &
+#docker logs -f ${NAME}_worker &
 
-read -n1 -r -p "Press a key to continue..." key
+#read -n1 -r -p "Press a key to continue..." key
