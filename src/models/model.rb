3706a5e4adb58e4a19709095465b5dfb5c7f9feb
Ryan Barth <ryan@barth.tech>
2017-12-01 11:05:36 -0800

    update prepaid and current products

--- src/models/model.rb
+++ src/models/model.rb
@@ -25,6 +25,8 @@ class Subscription < ActiveRecord::Base
     { id: '9421067602', title: '3 MONTHS' },
     { id: '9109818066', title: 'VIP 3 Month Box' },
     { id: '9175678162', title: 'VIP 3 Monthly Box' },
+    { id: '52386037778', title: 'Go Time - 3 Item' },
+    { id: '69026938898', title: 'Power Moves - 3 Item' },
   ].freeze
 
   #for changing sizes
@@ -36,6 +38,10 @@ class Subscription < ActiveRecord::Base
     { id: '9109818066', title: 'VIP 3 Month Box' },
     { id: '10016265938', title: 'Ellie 3- Pack: ' },
     { id: '10870682450', title: 'Fit to Be Seen Ellie 3- Pack' },
+    { id: '52386037778', title: 'Go Time - 3 Item' },
+    { id: '44383469586', title: 'Go Time - 5 Item' },
+    { id: '69026938898', title: 'Power Moves - 3 Item' },
+    { id: '69026316306', title: 'Power Moves - 5 Item' },
   ].freeze
 
   #for skips/alternates
