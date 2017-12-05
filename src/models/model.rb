9ff7fc894890b2c9d6122a9213238f8b1e2f1f27
Floyd Wallace <floyd@ip-172-31-1-95.ec2.internal>
2017-12-05 01:08:57 +0000

    update current products for autorenew

--- src/models/model.rb
+++ src/models/model.rb
@@ -36,14 +36,15 @@ class Subscription < ActiveRecord::Base
     { id: '9109818066', title: 'VIP 3 Month Box' },
     { id: '10016265938', title: 'Ellie 3- Pack: ' },
     { id: '10870682450', title: 'Fit to Be Seen Ellie 3- Pack' },
+    { id: '44383469586', title: 'Go Time' },
     { id: '52386037778', title: 'Go Time - 3 Item' },
-    { id: '44383469586', title: 'Go Time - 5 Item' },
-    { id: '78548729874', title: 'Go Time - 3 Item  Auto renew'},
-    { id: '78681669650', title: 'Go Time - 5 Item  Auto renew'},
-    { id: '78541520914', title: 'Power Moves - 3 Item  Auto renew'},
-    { id: '78657093650', title: 'Power Moves - 5 item  Auto renew'},
+    { id: '78548729874', title: 'Go Time - 3 Item  Auto renew' },
+    { id: '78480408594', title: 'Go Time - 5 Item' },
+    { id: '78681669650', title: 'Go Time - 5 Item  Auto renew' },
     { id: '69026938898', title: 'Power Moves - 3 Item' },
+    { id: '78541520914', title: 'Power Moves - 3 Item  Auto renew' },
     { id: '69026316306', title: 'Power Moves - 5 Item' },
+    { id: '78657093650', title: 'Power Moves - 5 item  Auto renew' },
   ].freeze
 
   #for skips/alternates
