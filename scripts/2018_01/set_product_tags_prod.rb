a0897c59ab524d10eb5954ddece3666d517136c0
Ryan Barth <ryan@barth.tech>
2017-12-29 11:55:58 -0800

    add 2018_01 production product tags script

--- scripts/2018_01/set_product_tags_prod.rb
+++ scripts/2018_01/set_product_tags_prod.rb
@@ -2,4 +2,14 @@
 
 require_relative '../pry_context'
 
-#ProductTag.find_or_create_by(product_id: , tag: , active_start: Date.)
+# fit and fierce
+ProductTag.find_or_create_by(product_id: 91235975186, tag: 'current', active_start: Time.new(2018, 1, 1, 0, 0, 0, '+08:00'))
+ProductTag.find_or_create_by(product_id: 91236171794, tag: 'current', active_start: Time.new(2018, 1, 1, 0, 0, 0, '+08:00'))
+ProductTag.find_or_create_by(product_id: 91235975186, tag: 'skippable', active_start: Time.new(2018, 1, 1, 0, 0, 0, '+08:00'))
+ProductTag.find_or_create_by(product_id: 91236171794, tag: 'skippable', active_start: Time.new(2018, 1, 1, 0, 0, 0, '+08:00'))
+ProductTag.find_or_create_by(product_id: 91235975186, tag: 'switchable', active_start: Time.new(2018, 1, 1, 0, 0, 0, '+08:00'))
+ProductTag.find_or_create_by(product_id: 91236171794, tag: 'switchable', active_start: Time.new(2018, 1, 1, 0, 0, 0, '+08:00'))
+
+# alt product, modern muse
+ProductTag.find_or_create_by(product_id: 91236368402, tag: 'current', active_start: Time.new(2018, 1, 1, 0, 0, 0, '+08:00'))
+ProductTag.find_or_create_by(product_id: 91236466706, tag: 'current', active_start: Time.new(2018, 1, 1, 0, 0, 0, '+08:00'))
