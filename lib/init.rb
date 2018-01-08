dc2035818ff4b50fdf038fe807d38064d276e71b
Floyd Wallace <floyd@ip-172-31-1-95.ec2.internal>
2018-01-08 19:52:34 +0000

    use bundler/setup

--- lib/init.rb
+++ lib/init.rb
@@ -1,5 +1,6 @@
-require 'bundler'
-Bundler.require(:default, ENV['RACK_ENV'])
+#require 'bundler'
+#Bundler.require(:default, ENV['RACK_ENV'])
+require 'bundler/setup'
 Dir[File.dirname(__FILE__) + '/../lib/**/*.rb'].each do |file|
   require file
 end
