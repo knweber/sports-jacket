4d9925876fb02595972b22547e1d131b297cc968
Ryan Barth <ryan@barth.tech>
2018-01-23 16:29:20 -0800

    add product tag scripts for febuary

---
+++ scripts/2018_02/set_product_tags_prod.rb
@@ -0,0 +1,28 @@
+#!/bin/ruby
+
+require_relative '../pry_context'
+
+month_start = Time.local(2018, 2)
+month_end = month_start.end_of_month
+base_tag = { active_start: month_start, active_end: month_end }
+
+# sunset the old tags without an active_end
+ProductTag.where(active_end: nil).update_all(active_end: month_start - 1.second)
+
+# main: La Vie en Rose
+main_3 = 138427301906
+main_5 = 138427203602
+ProductTag.find_or_create_by(base_tag.merge(product_id: alt1_3, tag: 'current'))
+ProductTag.find_or_create_by(base_tag.merge(product_id: alt1_5, tag: 'current'))
+
+# alt 1
+alt1_3 = 138427596818
+alt1_5 = 138427465746
+ProductTag.find_or_create_by(base_tag.merge(product_id: alt1_3, tag: 'current'))
+ProductTag.find_or_create_by(base_tag.merge(product_id: alt1_5, tag: 'current'))
+
+# alt 2
+alt2_3 = 138427793426
+alt2_5 = 138427695122
+ProductTag.find_or_create_by(base_tag.merge(product_id: alt2_3, tag: 'current'))
+ProductTag.find_or_create_by(base_tag.merge(product_id: alt2_5, tag: 'current'))
